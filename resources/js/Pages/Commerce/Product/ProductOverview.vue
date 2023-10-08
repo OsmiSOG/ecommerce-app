f31f99dfccecf817e800ae8fcca24fc6ab64ddd4
OsmiSOG <santiago.ooo6@gmail.com>
2023-10-07 21:39:59 -0500

    run build

--- resources/js/Pages/Commerce/Product/ProductOverview.vue
+++ resources/js/Pages/Commerce/Product/ProductOverview.vue
@@ -72,7 +72,7 @@ onMounted(() => {
                         <p class="text-3xl tracking-tight text-gray-900">{{NumberFormat(product.price)}}</p>
 
                         <form class="mt-10">
-                            <Link :href="route('cart.add', product.id)" method="post" as="button" :on-success="Swal.fire('Added', 'added to bag', 'success')" preserve-scroll class="mt-10 flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+                            <Link :href="route('cart.add', product.id)" method="post" as="button" @success="Swal.fire('Added', 'added to bag', 'success')" preserve-scroll class="mt-10 flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
                                 Add to bag
                             </Link>
                         </form>
